8232fbe4878209a43647768a85755d7a5aa30383
lei0lei <lei.lei.fan.meng@gmail.com>
2025-05-09 11:41:46 +0800

    .

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -519,8 +519,8 @@ pub fn generate_depth_result_trigger(action1: &[f64], action2: &[f64], hole_conf
 
 // 获取直径结果
 pub fn generate_diameter_result(diameter: &HoleDiameter, hole_config: &HoleConfig) -> (f64, bool) {
-  let real_diameter = diameter.nei_diameter * 0.01;
-  let is_ok = (real_diameter >= hole_config.diameter_min-0.03) && (real_diameter <= hole_config.diameter_max+0.03);
+  let real_diameter = diameter.nei_diameter * 0.0102;
+  let is_ok = (real_diameter >= hole_config.diameter_min-0.05) && (real_diameter <= hole_config.diameter_max+0.05);
   (real_diameter, is_ok)
 }
 
